48a0a9fc67951e4d910a0360b85866e300506aaf
Kumar Jyotirmay <jmy2898@gmail.com>
2026-01-28 14:45:16 +0530

    refactor: remove unused Currency type import in currencies test

--- src/lib/__test__/currencies.test.ts
+++ src/lib/__test__/currencies.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { CURRENCIES, type Currency } from "../currencies";
+import { CURRENCIES } from "../currencies";
 
 describe("currencies", () => {
     it("exports CURRENCIES array", () => {
